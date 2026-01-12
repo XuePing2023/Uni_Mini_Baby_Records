e49d42e62cc554f201eba6d3d526c679c8f3a57e
Hawking <hawking202108@gmail.com>
2026-01-12 15:03:05 +0800

    feat: init project

---
+++ src/pages/records/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'Records',
+  options: {
+    virtualHost: true,
+  },
+})
+definePage({
+  style: {
+    navigationBarTitleText: '%records.record%',
+  },
+})
+</script>
+
+<template>
+  <view class="">
+    Test
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
